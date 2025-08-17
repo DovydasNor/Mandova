101b58b9be912981b90e5f266ff3c151d838f3ef
DovydasN <klimpau741@gmail.com>
2025-08-17 17:01:05 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
-import Home from './pages/home';
+import Home from './pages/Home';
 import Gallery from './pages/Gallery';
 import Contacts from './pages/Contacts';
 import NotFoundPage from './pages/404_Page';
